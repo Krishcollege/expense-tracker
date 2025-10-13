ac4fbd15b3659dde7fbc5d15b1d177d832d64c71
krishtopiwala <2024041608@vnsgu.ac.in>
2025-10-13 23:09:08 +0530

    updated commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+const authRoutes = require("./routes/authRoutes.js");
 
 const app = express();
 
@@ -18,6 +20,10 @@ app.use(
 );
 
 app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
